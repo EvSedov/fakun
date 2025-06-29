5b35786ae4c332e17b6e8fe49510f765f1a5a241
sedovev <sedovev@rgn.gov.ru>
2025-06-29 20:29:17 +0700

    changes style for Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,10 @@
     <!-- Зеленая линия сверху -->
     <div class="absolute top-0 left-0 w-full h-[2px] bg-[#72C95E]"></div>
     <div
-      class="max-w-6xl mx-auto flex flex-col md:flex-row md:justify-between gap-10 md:gap-0"
+      class="max-w-6xl mx-auto flex flex-col md:flex-row md:justify-between gap-10 md:gap-0 items-center"
     >
       <!-- Контакты (по левому краю) -->
-      <div class="flex flex-col gap-4 min-w-[260px] items-start">
+      <div class="flex flex-col gap-2.5 min-w-[260px] items-start">
         <img
           :src="footerCompanyLogo"
           alt="Fakun logo"
@@ -16,30 +16,49 @@
           >Контакты</span
         >
         <div class="flex items-center gap-2">
-          <img :src="flagRussia" alt="Флаг России" class="w-6 h-4 rounded-sm" />
-          <span class="text-[20px] text-[#72C95E] font-evolventa font-normal"
-            >+7 993 281 12 65</span
-          >
+          <a href="tel:+79932811265" class="flex items-center gap-2">
+            <img
+              :src="flagRussia"
+              alt="Флаг России"
+              class="w-6 h-4 rounded-sm"
+            />
+            <span
+              class="text-[20px] text-[#72C95E] font-evolventa font-normal hover:text-white"
+              >+7 993 281 12 65</span
+            >
+          </a>
           <img :src="whatsapp" alt="WhatsApp" class="w-5 h-5" />
           <img :src="telegram" alt="Telegram" class="w-5 h-5" />
         </div>
         <div class="flex items-center gap-2">
-          <img :src="flagSpain" alt="Флаг Испании" class="w-6 h-4 rounded-sm" />
-          <span class="text-[20px] text-[#72C95E] font-evolventa font-normal"
-            >+34 636 06 38 66</span
-          >
+          <a href="tel:+34636063866" class="flex items-center gap-2">
+            <img
+              :src="flagSpain"
+              alt="Флаг Испании"
+              class="w-6 h-4 rounded-sm"
+            />
+            <span
+              class="text-[20px] text-[#72C95E] font-evolventa font-normal hover:text-white"
+              >+34 636 06 38 66</span
+            >
+          </a>
           <img :src="whatsapp" alt="WhatsApp" class="w-5 h-5" />
           <img :src="telegram" alt="Telegram" class="w-5 h-5" />
           <span class="text-[14px] text-white font-evolventa"
             >(говорим по-русски)</span
           >
         </div>
-        <span class="text-[14px] text-white font-evolventa"
-          >Email: padelfakun@yandex.ru</span
-        >
-        <span class="text-[14px] text-white font-evolventa"
-          >Часы работы: Пн-Пт 9:00-21:00, Сб-Вс 10:00-19:00</span
-        >
+        <div class="flex flex-col">
+          <a href="mailto:padelfakun@yandex.ru" class="m-0 p-0">
+            <span
+              class="text-[14px] text-white font-evolventa hover:text-[#72C95E]"
+              >Email: padelfakun@yandex.ru</span
+            >
+          </a>
+          <span class="text-[14px] text-white font-evolventa"
+            >Часы работы: Пн-Пт 9:00-21:00, Сб-Вс 10:00-19:00</span
+          >
+        </div>
       </div>
       <!-- Компания -->
       <div class="flex flex-col gap-4 min-w-[120px]">
@@ -59,26 +78,40 @@
       </div>
 
       <!-- Социальные сети и маркетплейс -->
-      <div class="flex flex-col gap-4 min-w-[180px] items-start">
-        <span class="font-bold text-[16px] text-white font-evolventa"
-          >Социальные сети</span
-        >
-        <div class="flex items-center gap-2">
-          <img :src="telegram" alt="Telegram" class="w-5 h-5" />
-          <img :src="instagram" alt="Instagram" class="w-5 h-5" />
+      <div class="flex flex-col gap-7.5 min-w-[180px] items-start">
+        <div class="flex flex-col gap-2.5">
+          <span class="font-bold text-[16px] text-white font-evolventa"
+            >Социальные сети</span
+          >
+          <div class="flex items-center gap-2">
+            <img
+              :src="telegram"
+              alt="Telegram"
+              class="w-5 h-5 hover:brightness-0 hover:invert"
+            />
+            <img
+              :src="instagram"
+              alt="Instagram"
+              class="w-5 h-5 hover:brightness-0 hover:invert"
+            />
+          </div>
         </div>
-        <span class="font-bold text-[16px] text-white font-evolventa mt-4"
-          >Наши ракетки на маркетплейс</span
-        >
-        <a
-          href="#"
-          class="inline-block border border-[#005BFF] rounded-[5px] px-4 py-2 text-[#005BFF] font-evolventa text-[16px] hover:bg-[#005BFF] hover:text-white transition"
-          >OZON</a
-        >
+        <div class="flex flex-col gap-2.5">
+          <span class="font-bold text-[16px] text-white font-evolventa"
+            >Наши ракетки на маркетплейс</span
+          >
+          <a
+            href="#"
+            class="max-w-18 inline-block border border-[#005BFF] rounded-[5px] px-2.5 py-0.5 text-white font-evolventa text-[16px] hover:bg-[#005BFF] hover:text-white transition"
+            >OZON</a
+          >
+        </div>
       </div>
     </div>
     <!-- Копирайт -->
-    <div class="mt-8 text-center text-[12px] text-white/80 font-evolventa">
+    <div
+      class="relative mt-8 text-center text-[12px] text-white/80 font-evolventa border-t-[1px] border-white/30 pt-7"
+    >
       © 2025 Fakun. Все права защищены. Информация сайта защищена законом об
       авторских правах.
     </div>
