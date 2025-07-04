604a7c379bb51e437f665afb807dbbe9b3ac6617
sedovev <sedovev@rgn.gov.ru>
2025-07-05 00:40:33 +0700

    adds RacketDetailsModal

--- src/components/FakunRackets.vue
+++ src/components/FakunRackets.vue
@@ -12,7 +12,9 @@
         class="flex items-center justify-center gap-6 max-w-[1200px] mx-auto relative z-10"
       >
         <button
-          class="flex items-center justify-center w-14 h-14 rounded-full border border-[#72C95E] bg-transparent shadow-md mr-2 hover:bg-[#72C95E33] cursor-pointer"
+          class="flex items-center justify-center min-w-14 h-14 rounded-full border border-[#72C95E] bg-transparent shadow-md mr-2 hover:bg-[#72C95E33] cursor-pointer"
+          @click="prevRackets"
+          :disabled="currentIndex === 0"
         >
           <svg
             width="24"
@@ -25,15 +27,23 @@
             <path d="M15 6l-6 6 6 6" />
           </svg>
         </button>
-        <div class="flex gap-6">
-          <CardRacket
-            v-for="racket in rackets"
-            :key="racket.name"
-            :racket="racket"
-          />
+        <div class="flex overflow-hidden relative">
+          <div
+            class="flex gap-4 transition-transform duration-300 ease-in-out"
+            :style="{ transform: `translateX(${translateX}px)` }"
+          >
+            <CardRacket
+              v-for="racket in rackets"
+              :key="racket.id"
+              :racket="racket"
+              @click="openRacketDetails(racket)"
+            />
+          </div>
         </div>
         <button
-          class="flex items-center justify-center w-14 h-14 rounded-full border border-[#72C95E] bg-transparent shadow-md ml-2 hover:bg-[#72C95E33] cursor-pointer"
+          class="flex items-center justify-center min-w-14 h-14 rounded-full border border-[#72C95E] bg-transparent shadow-md ml-2 hover:bg-[#72C95E33] cursor-pointer"
+          @click="nextRackets"
+          :disabled="currentIndex >= rackets.length - itemsPerPage"
         >
           <svg
             width="24"
@@ -48,38 +58,347 @@
         </button>
       </div>
     </div>
+    <RacketDetailsModal
+      v-if="selectedRacket"
+      :is-open="isModalOpen"
+      :racket="selectedRacket"
+      @close="closeRacketDetails"
+      @update:racket="handleRacketUpdate"
+    />
   </section>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref, computed } from "vue";
 import CardRacket from "./CardRacket.vue";
+import RacketDetailsModal from "./RacketDetailsModal.vue";
+
 import racketX31Img from "@/assets/images/racket-x31.png";
 import racketAbaddonImg from "@/assets/images/racket-abaddon.png";
 import racketAzraelImg from "@/assets/images/racket-azrael.png";
+import racketAstarothImg from "@/assets/images/racket-astaroth.png";
+import racketJerichoImg from "@/assets/images/racket-jericho.png";
 
-const rackets = [
+interface Racket {
+  id: string;
+  name: string;
+  price: string;
+  image: string;
+  colors: (string | [string, string])[];
+  selectedColor: string | [string, string];
+  ozonLink: string;
+  description: string;
+  characteristics: {
+    geometry: string;
+    profile: string;
+    balance: string;
+    weight: string;
+    level: string;
+    [key: string]: string;
+  };
+  materials: {
+    mainMaterial: string;
+    coreComposition: string;
+    productionTechnology: string;
+    drilling: string;
+    protector: string;
+    [key: string]: string;
+  };
+  gameFeatures: {
+    control: string;
+    power: string;
+    accuracy: string;
+    durability: string;
+    [key: string]: string;
+  };
+  advantages: {
+    technological: string;
+    versatility: string;
+    efficiency: string;
+    longevity: string;
+    [key: string]: string;
+  };
+}
+
+const isModalOpen = ref(false);
+const selectedRacket = ref<Racket | null>(null);
+
+const currentIndex = ref(0);
+const itemsPerPage = 3;
+const cardWidth = 300; // Примерная ширина карточки в px
+const gapWidth = 24; // Ширина gap в px (gap-6 = 24px)
+const translateX = ref(0);
+
+const nextRackets = () => {
+  if (currentIndex.value < rackets.value.length - itemsPerPage) {
+    currentIndex.value++;
+    translateX.value = -(currentIndex.value * (cardWidth + gapWidth));
+  }
+};
+
+const prevRackets = () => {
+  if (currentIndex.value > 0) {
+    currentIndex.value--;
+    translateX.value = -(currentIndex.value * (cardWidth + gapWidth));
+  }
+};
+
+const rackets = ref<Racket[]>([
   {
+    id: "racket-x31",
     name: "FAKUN X31",
     image: racketX31Img,
-    colors: ["#000000"],
-    ozonLink: "#",
-    price: "34 990",
+    colors: ["#000000", ["#FFFFFF", "#FF0000"], ["#0000FF", "#FFD700"]],
+    selectedColor: "#000000",
+    ozonLink:
+      "https://www.ozon.ru/product/raketka-dlya-padel-tennisa-fakun-x31-black-edition-s-sherohovatoy-poverhnostyu-2060278771/?at=GRt2rol94TX6k5BZCoXLr5FVxWXvoI70X463iANkY2Z",
+    price: "35 000",
+    description: `FAKUN X31 представляет собой флагманскую модель
+                  компании FAKUN, существующую на рынке более 10 лет. В
+                  версии "ABADDON" модель достигла совершенства
+                  благодаря десятилетнему развитию и тщательной
+                  проработке каждой детали, превратившись в настоящее
+                  произведение функционального искусства.`,
+    characteristics: {
+      geometry: "каплевидная / негабаритная",
+      profile: "38 мм",
+      balance: "Центральный",
+      weight: "360 грамм",
+      level: "высокий / профессиональный",
+    },
+    materials: {
+      mainMaterial: "цельный CARBON SMT + 6 слоев стекловолокна FIBERGLASS",
+      coreComposition: "EVA SOFT",
+      productionTechnology: `DOUBLE REVERSE LAMINATION - уникальная система
+                             производства, обеспечивающая идеальный динамический
+                             баланс благодаря точному соотношению смолы и волокон`,
+      drilling: `SIMETRIC ORBIT CONCEPT - орбитальная центровка
+                 сверления, разработанная на основе детального анализа
+                 гибкости и реакции`,
+      protector: "периферийная защита (в комплекте)",
+      reinforcement: `Углеродное усиление - стратегически расположенные
+                      элементы, обеспечивающие жесткость в ключевых точках,
+                      предотвращающие деформацию при ударе и увеличивающие
+                      долговечность ракетки`,
+    },
+    gameFeatures: {
+      control: "отличная маневренность",
+      power: "высокая скорость передвижения",
+      accuracy: "точное размещение мяча",
+      durability:
+        "очень высокая благодаря используемой технологии производства",
+    },
+    advantages: {
+      technological: `Эволюционный дизайн (EVOLUTION + REVOLUTION)`,
+      versatility: `Маневренность и взрывная сила удара`,
+      efficiency: `Мощность и точность`,
+      longevity: `Идеальное соотношение формы и функциональности`,
+    },
   },
   {
+    id: "racket-abaddon",
     name: "FAKUN ABADDON",
     image: racketAbaddonImg,
-    colors: ["#FF0000", "#FFC62A"],
+    colors: [["#FF0000", "#FFD700"], ["#FFFFFF", "#FF0000"], "#FFC62A"],
+    selectedColor: ["#FF0000", "#FFD700"],
     ozonLink: "#",
     price: "32 990",
+    description: `Ракетка FAKUN ASTAROTH представляет собой
+                  высокотехнологичное решение для игроков высокого
+                  уровня, стремящихся к максимальной эффективности в
+                  игре, благодаря инновационным материалам и продуманной
+                  конструкции.`,
+    characteristics: {
+      geometry: "Каплевидная / Негабаритная",
+      profile: "38 мм",
+      balance: "Центральный",
+      weight: "355 г",
+      level: "Высокий / Профессиональный",
+      protectiveCoating: "Встроенная периферийная защита",
+    },
+    materials: {
+      mainMaterial: "Цельный карбон SMT + 6 слоев стекловолокна",
+      coreComposition: "EVA Soft Attack",
+      productionTechnology: "Double Reverse Lamination",
+      drilling: "Symmetric Orbit Concept",
+      protector: "Встроенная периферийная защита",
+    },
+    gameFeatures: {
+      control: "Высокая маневренность позволяет эффективно играть в защите",
+      power: "Идеальна для мощных и точных ударов",
+      accuracy: "Позволяет точно размещать мяч в желаемых зонах корта",
+      durability:
+        "Очень высокая благодаря используемой технологии производства",
+    },
+    advantages: {
+      technological: "Использование материалов военной промышленности",
+      versatility: "Идеальное сочетание для агрессивной игры",
+      efficiency: "Высокая скорость передвижения ракетки",
+      longevity: "Исключительная надежность конструкции",
+    },
   },
   {
+    id: "racket-azrael",
     name: "FAKUN AZRAEL",
     image: racketAzraelImg,
-    colors: ["#FF0000", "#000000"],
+    colors: [
+      ["#FF0000", "#FFC62A"],
+      ["#000000", "#FFFFFF"],
+    ],
+    selectedColor: ["#FF0000", "#FFC62A"],
     ozonLink: "#",
     price: "34 990",
+    description: `FAKUN AZRAEL - это высокотехнологичное решение для
+                  игроков высокого уровня, ценящих тактическую игру и
+                  точность. Инновационные материалы и продуманная
+                  конструкция делают эту ракетку идеальным выбором для
+                  профессионалов, стремящихся к максимальной
+                  эффективности на корте.`,
+    characteristics: {
+      geometry: "Круглая",
+      profile: "38 мм",
+      balance: "Центральный",
+      weight: "360-365 грамм",
+      level: "Высокий/профессиональный",
+    },
+    materials: {
+      mainMaterial:
+        "Высококачественные материалы из авиакосмической промышленности",
+      coreComposition: "Оптимальное распределение упругости",
+      productionTechnology: "DOUBLE REVERSE LAMINATION",
+      drilling: "Орбитальное центровочное с симметричной концепцией",
+      protector: "Встроенные периферийные протекторные элементы",
+    },
+    gameFeatures: {
+      control: "Максимально тактичная",
+      power: "Оптимальное распределение упругости",
+      accuracy: "Простая в использовании",
+      durability: "Очень высокая благодаря системе производства",
+    },
+    advantages: {
+      technological:
+        "Уникальная особенность ракетки заключается в отсутствии дополнительной косметики.",
+      versatility:
+        "Ракетка сохраняет первоначальный вид благодаря интегрированной технологии печати.",
+      efficiency: "Орбитальное центровочное с симметричной концепцией",
+      longevity: "Встроенные периферийные протекторные элементы",
+    },
   },
-];
+  {
+    id: "racket-astaroth",
+    name: "FAKUN ASTAROTH",
+    image: racketAstarothImg,
+    colors: [
+      "#000000",
+      ["#156824", "#FFC62A"],
+      ["#FF0000", "#FFFFFF"],
+      ["#005BFF", "#FFC62A"],
+    ],
+    selectedColor: "#000000",
+    ozonLink:
+      "https://www.ozon.ru/product/raketka-dlya-padel-tennisa-fakun-astaroth-s-sherohovatoy-poverhnostyu-2060298115/?at=Brtzx3mNKSOElqKMTzrvVl9Ty5ArkOtNMkXKguVE7RM6",
+    price: "35 000",
+    description: `Ракетка FAKUN ASTAROTH представляет собой
+                  высокотехнологичное решение для игроков высокого
+                  уровня, стремящихся к максимальной эффективности в
+                  игре, благодаря инновационным материалам и продуманной
+                  конструкции.`,
+    characteristics: {
+      geometry: "Каплевидная / Негабаритная",
+      profile: "38 мм",
+      balance: "Центральный",
+      weight: "360 грамм",
+      level: "Высокий / Профессиональный",
+    },
+    materials: {
+      mainMaterial: "Цельный CARBON SMT + 6 слоев стекловолокна FIBERGLASS",
+      coreComposition:
+        "Смесь ADVANCE (EVA SOFT ATACK + NEW RED PRO + EVA SOFT ATACK)",
+      productionTechnology: "DOUBLE REVERSE LAMINATION",
+      drilling: "Орбитальное центровочное с симметричной концепцией",
+      protector: "Периферийная защита (в комплекте)",
+    },
+    gameFeatures: {
+      control: "Точный",
+      power: "Экстраординарная благодаря центру RED PRO",
+      accuracy: "Высокая, с возможностью выполнения быстрых подач и эффектов",
+      durability: "Очень высокая благодаря системе производства",
+    },
+    advantages: {
+      technological:
+        "Использование материалов последнего поколения из авиакосмической промышленности",
+      versatility:
+        "Подходит как для профессиональных игроков, так и для продвинутых любителей",
+      efficiency:
+        "Обеспечивает максимальную мощность при сохранении точного контроля",
+      longevity:
+        "Отсутствие внешних покрытий гарантирует сохранение первоначального вида.",
+    },
+  },
+  {
+    id: "racket-jericho",
+    name: "FAKUN JERICHO",
+    image: racketJerichoImg,
+    colors: ["#000000"],
+    selectedColor: "#000000",
+    ozonLink:
+      "https://www.ozon.ru/product/raketka-dlya-padel-tennisa-fakun-jericho-2055755219/?at=qQtJgGrvNc3k6ZGWCAkR1P2u9zEmy7IJV41v0TrDrJRM",
+    price: "31 500",
+    description: `Ракетка FAKUN JERICHO - это результат многолетнего
+                  развития модели, которая с 2012 года остается верной
+                  своему принципу быть "ракеткой, которая нравится
+                  всем". Она не специализируется на каком-то одном типе
+                  удара, но демонстрирует отличные характеристики во
+                  всех аспектах игры.`,
+    characteristics: {
+      geometry: "Круглая",
+      profile: "38 мм",
+      balance: "Низкий",
+      weight: "330 г",
+      level: "Подходит для всех уровней",
+      protectiveCoating: "Встроенная периферийная защита",
+    },
+    materials: {
+      mainMaterial: "Интегральный карбон SMT + 6 слоев стекловолокна",
+      coreComposition: "Сверхмягкая резина",
+      productionTechnology: "Tubular FIBERFIRE + CARBON AXIAL",
+      drilling: "Стратегические карбоновые стойки",
+      protector: "Встроенные периферийные протекторные элементы",
+    },
+    gameFeatures: {
+      control:
+        "Пятно по центру обеспечивает сбалансированное ощущение при каждом ударе",
+      power: "Универсальность: Подходит для любого стиля игры",
+      accuracy: "Широкая зона поражения для надежной защиты и атаки",
+      durability: "Использование материалов аэрокосмической промышленности",
+    },
+    advantages: {
+      technological:
+        "Отсутствие дополнительных покрытий (шпатлевок, красок, наклеек) сохраняет идеальный вес и баланс.",
+      versatility:
+        "Идеальное распределение веса благодаря точному соотношению смолы и волокон",
+      efficiency: "Оптимальное соотношение жесткости и гибкости",
+      longevity: "Благодаря продуманной балансировке в кулаке",
+    },
+  },
+]);
+
+const openRacketDetails = (racket: Racket) => {
+  selectedRacket.value = racket;
+  isModalOpen.value = true;
+};
+
+const closeRacketDetails = () => {
+  isModalOpen.value = false;
+  selectedRacket.value = null;
+};
+
+const handleRacketUpdate = (updatedRacket: Racket) => {
+  const index = rackets.value.findIndex((r) => r.id === updatedRacket.id);
+  if (index !== -1) {
+    rackets.value[index] = updatedRacket;
+  }
+};
 </script>
 
 <style scoped>
---
+++ src/components/RacketDetailsModal.vue
@@ -0,0 +1,252 @@
+<template>
+  <div
+    v-if="isOpen"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black/70 p-4"
+    @click.self="closeModal"
+  >
+    <div class="p-3">
+      <div
+        class="bg-[#2A2A2A] rounded-[10px] shadow-lg max-w-[1000px] w-full max-h-[90vh] relative flex flex-col md:flex-row gap-12.5 px-10 py-15"
+      >
+        <!-- Кнопка закрытия -->
+        <button
+          class="absolute top-[-10px] right-[-10px] text-white w-8 h-8 rounded-full flex items-center justify-center hover:bg-gray-700 transition-colors z-55 border-white border-2"
+          @click="closeModal"
+        >
+          <svg
+            class="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
+        </button>
+        <!-- Левая часть: Изображение ракетки, кнопки, описание -->
+        <div class="max-w-[398px] flex flex-col items-center justify-start">
+          <div
+            class="bg-white overflow-hidden w-full h-auto flex items-center justify-center mb-6 rounded-[10px] px-14.5"
+          >
+            <img
+              :src="racket.image"
+              :alt="racket.name"
+              class="max-w-[282px] w-full h-auto object-contain"
+            />
+          </div>
+
+          <div class="flex gap-4 mb-6 justify-start items-center w-full">
+            <button
+              class="px-5 py-3 rounded-[5px] border border-[#72C95E] text-white font-evolventa text-[16px] flex items-center justify-center hover:bg-[#72C95E] hover:text-black transition-colors duration-200"
+            >
+              Добавить в корзину
+            </button>
+            <a
+              :href="racket.ozonLink"
+              target="_blank"
+              class="px-5 py-3 rounded-[5px] border border-[#005BFF] text-white font-evolventa text-[16px] flex items-center justify-center hover:bg-[#005BFF] transition-colors duration-200"
+            >
+              OZON
+            </a>
+          </div>
+
+          <p class="text-white font-evolventa text-[14px]">
+            {{ racket.description }}
+          </p>
+        </div>
+
+        <!-- Правая часть: Детали и характеристики -->
+        <div class="w-full text-white">
+          <div class="flex justify-between items-center mb-2.5">
+            <h2 class="text-[#72C95E] text-[20px]">
+              {{ racket.name }}
+            </h2>
+            <!-- Цветовые опции -->
+            <div class="flex gap-3">
+              <div
+                v-for="(colorOption, index) in racket.colors"
+                :key="index"
+                class="relative w-7 h-7 rounded-full shadow-md cursor-pointer flex-shrink-0 flex items-center justify-center"
+                :class="{
+                  'border-2 border-white':
+                    (Array.isArray(colorOption) &&
+                      Array.isArray(racket.selectedColor) &&
+                      colorOption[0] === racket.selectedColor[0] &&
+                      colorOption[1] === racket.selectedColor[1]) ||
+                    (!Array.isArray(colorOption) &&
+                      !Array.isArray(racket.selectedColor) &&
+                      colorOption === racket.selectedColor),
+                }"
+                @click="selectColor(colorOption)"
+              >
+                <div
+                  v-if="!Array.isArray(colorOption)"
+                  class="w-full h-full rounded-full"
+                  :style="{ backgroundColor: colorOption }"
+                ></div>
+                <div
+                  v-else
+                  class="relative w-full h-full rounded-full flex items-center justify-center"
+                  :style="{ backgroundColor: colorOption[0] }"
+                >
+                  <div
+                    class="w-4 h-4 rounded-full"
+                    :style="{ backgroundColor: colorOption[1] }"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h3 class="text-[#72C95E] text-[20px] mb-5">{{ racket.price }} ₽</h3>
+
+          <!-- Секции характеристик -->
+          <div class="space-y-2.5 text-[14px]">
+            <div v-if="racket.characteristics">
+              <p class="text-[#72C95E] text-[16px]">Основные характеристики</p>
+              <ul class="list-disc pl-5 text-[14px]">
+                <li>Геометрия: {{ racket.characteristics.geometry }}</li>
+                <li>Профиль: {{ racket.characteristics.profile }}</li>
+                <li>Баланс: {{ racket.characteristics.balance }}</li>
+                <li>Вес: {{ racket.characteristics.weight }}</li>
+                <li>Уровень: {{ racket.characteristics.level }}</li>
+              </ul>
+            </div>
+
+            <div v-if="racket.materials">
+              <p class="text-[#72C95E] text-[16px]">Материалы и конструкция</p>
+              <ul class="list-disc pl-5 text-[14px]">
+                <li>Основной материал: {{ racket.materials.mainMaterial }}</li>
+                <li>Состав ядра: {{ racket.materials.coreComposition }}</li>
+                <li>
+                  Технология производства:
+                  {{ racket.materials.productionTechnology }}
+                </li>
+                <li>Бурение: {{ racket.materials.drilling }}</li>
+                <li>Протектор: {{ racket.materials.protector }}</li>
+              </ul>
+            </div>
+
+            <div v-if="racket.gameFeatures">
+              <p class="text-[#72C95E] text-[16px]">Игровые характеристики</p>
+              <ul class="list-disc pl-5 text-[14px]">
+                <li>Контроль: {{ racket.gameFeatures.control }}</li>
+                <li>Мощность: {{ racket.gameFeatures.power }}</li>
+                <li>Точность мяча: {{ racket.gameFeatures.accuracy }}</li>
+                <li>Прочность: {{ racket.gameFeatures.durability }}</li>
+              </ul>
+            </div>
+
+            <div v-if="racket.advantages">
+              <p class="text-[#72C95E] text-[16px]">Преимущества модели</p>
+              <ul class="list-disc pl-5 text-[14px]">
+                <li>Технологичность: {{ racket.advantages.technological }}</li>
+                <li>Универсальность: {{ racket.advantages.versatility }}</li>
+                <li>Эффективность: {{ racket.advantages.efficiency }}</li>
+                <li>Долговечность: {{ racket.advantages.longevity }}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, type PropType, ref, watch } from "vue";
+
+interface Racket {
+  id: string;
+  name: string;
+  price: string;
+  image: string;
+  colors: (string | [string, string])[];
+  selectedColor: string | [string, string];
+  ozonLink: string;
+  description: string;
+  characteristics: {
+    geometry: string;
+    profile: string;
+    balance: string;
+    weight: string;
+    level: string;
+  };
+  materials: {
+    mainMaterial: string;
+    coreComposition: string;
+    productionTechnology: string;
+    drilling: string;
+    protector: string;
+  };
+  gameFeatures: {
+    control: string;
+    power: string;
+    accuracy: string;
+    durability: string;
+  };
+  advantages: {
+    technological: string;
+    versatility: string;
+    efficiency: string;
+    longevity: string;
+  };
+}
+
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+  racket: {
+    type: Object as PropType<Racket>,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["close", "update:racket"]);
+
+const currentRacket = ref<Racket>({ ...props.racket });
+
+// Обновляем currentRacket при изменении пропса racket
+watch(
+  () => props.racket,
+  (newVal) => {
+    currentRacket.value = { ...newVal };
+  },
+  { deep: true, immediate: true }
+);
+
+const closeModal = () => {
+  emit("close");
+};
+
+const selectColor = (color: string | [string, string]) => {
+  currentRacket.value.selectedColor = color;
+  emit("update:racket", currentRacket.value);
+};
+
+// Обработка Esc для закрытия модального окна
+const handleKeydown = (event: KeyboardEvent) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+};
+
+// Добавляем и убираем слушатель события
+import { onMounted, onUnmounted } from "vue";
+
+onMounted(() => {
+  document.addEventListener("keydown", handleKeydown);
+});
+
+onUnmounted(() => {
+  document.removeEventListener("keydown", handleKeydown);
+});
+</script>
+
+<style scoped></style>
